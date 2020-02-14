f369dc6177967216fb86f3b31917d2b7ea2b76c3
Cedric Duriau <duriau.cedric@live.be>
2020-02-15 00:13:56 +0100

    implement MathAdd

---
+++ fusion/Modules/Lua/mathutils.lua
@@ -0,0 +1,21 @@
+-- module table
+local mathutils = {}
+
+function mathutils.add(n1, n2)
+    --[[
+        Adds two numbers.
+
+        :param n1: Number to add to.
+        :type n1: number
+
+        :param n2: Number to add with.
+        :type n2: number
+
+        :rtype: number
+    ]]
+    local result = n1 + n2
+    return result
+end
+
+-- return module table
+return mathutils
